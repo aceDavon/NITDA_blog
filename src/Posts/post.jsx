c29f33a99c51822c79175f55505db40cc1635ab4
Maryamms <maryamshuaibu99@yahoo.com>
2024-04-24 10:47:51 +0100

    style popup modal

--- src/Posts/post.jsx
+++ src/Posts/post.jsx
@@ -1,30 +1,48 @@
-import { useState } from "react"
-import { useLocation } from "react-router-dom"
-import { EditPost } from "./editPosts"
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { EditPost } from "./editPosts";
+
 
 export function Post() {
-  const { state } = useLocation()
-  const [showModal, setShowModal] = useState(false)
+  const { state } = useLocation();
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+    document.body.classList.add("modal-open");
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    document.body.classList.remove("modal-open");
+  };
 
   return (
-    <div className="single-post-container">
-      <div className="post-header">{state.post.title}</div>
-      <div className="post-body">{state.post.body}</div>
-      <div className="post-id">{state.post.userId}</div>
-      <div className="post-btn-container">
-        <button className="delete-btn" type="button">
-          Delete Post
-        </button>
-        <button
-          onClick={() => setShowModal(!showModal)}
-          className="edit-btn"
-          id="edit"
-          type="button"
-        >
-          Edit Post
-        </button>
+    <div className="container">
+      <div className="single-post-container">
+        <div className="post-content">
+          <div className="post-header">{state.post.title}</div>
+          <div className="post-body">{state.post.body}</div>
+          <div className="post-id">User ID: {state.post.userId}</div>
+        </div>
+        <div className="post-btn-container">
+          <button className="delete-btn" type="button">
+            Delete Post
+          </button>
+          <button onClick={openModal} className="edit-btn" type="button">
+            Edit Post
+          </button>
+        </div>
       </div>
-      {showModal && <EditPost />}
+      {showModal && (
+        <div className="modal-overlay" onClick={closeModal}>
+            <span className="close-btn" onClick={closeModal}>
+              &times;
+            </span>
+            <EditPost />
+          
+        </div>
+      )}
     </div>
-  )
+  );
 }
